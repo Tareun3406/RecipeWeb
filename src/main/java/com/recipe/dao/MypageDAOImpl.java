ec167d68388b8bd8585de2b4105a50d448a23c2b
gukdongho <109472535+gukdongho@users.noreply.github.com>
2022-10-19 15:46:58 +0900

    mypage controller ver.2

--- src/main/java/com/recipe/dao/MypageDAOImpl.java
+++ src/main/java/com/recipe/dao/MypageDAOImpl.java
@@ -1,5 +1,4 @@
 package com.recipe.dao;
-
 import com.recipe.vo.mypageVO;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class MypageDAOImpl implements MypageDAO{
-
     @Autowired
     private SqlSession sqlSession;
-
-
     @Override
     public mypageVO getuser(String v2) {
         return this.sqlSession.selectOne("user_info",v2);
--- src/main/java/com/recipe/service/MypageService.java
+++ src/main/java/com/recipe/service/MypageService.java
@@ -1,10 +1,5 @@
 package com.recipe.service;
-
-
 import com.recipe.vo.mypageVO;
-
 public interface MypageService {
-
-
     mypageVO getuser(String v2);
 }

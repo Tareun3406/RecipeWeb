ec354d6c23a71c9374128ae22668e45b1b33a95f
nayoonjii <071446@naver.com>
2022-10-24 16:43:56 +0900

    댓글 수정폼 (번호값 안뜸)

--- src/main/java/com/recipe/service/CommuReplyService.java
+++ src/main/java/com/recipe/service/CommuReplyService.java
@@ -6,13 +6,15 @@ import com.recipe.vo.CommuReplyVO;
 
 public interface CommuReplyService {
 
+
 	void insertReply(CommuReplyVO vo);//추상메서드로 {}가없고, 호출이 불가능. 본문문장이없다. 해당 메서드 리턴타입 앞에 
 	//public abstract 가 생략됨.
-
+	
 	List<CommuReplyVO> listReply(int comu_no);
 
 	void updateReply(CommuReplyVO vo);
 
 	void deleteReply(int reply_no);
+
 
 }

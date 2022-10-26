eebf626b29828ce383609d34c5d44d5c50d8c169
khm81712 <khm81712@naver.com>
2022-10-26 16:24:39 +0900

    delete bootstrap

--- src/main/java/com/recipe/service/PostService.java
+++ src/main/java/com/recipe/service/PostService.java
@@ -1,10 +1,13 @@
 package com.recipe.service;
 
 import com.recipe.vo.PostVO;
+import com.recipe.vo.Recipe_PostDTO;
 
 import java.util.List;
 
 public interface PostService {
     List<PostVO> getPost(int post_no);
+
+    void insertRp(Recipe_PostDTO rpd);
 
 }

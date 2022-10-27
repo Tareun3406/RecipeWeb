98706a78ef14b146b322f42fbb3970e1fe1cce29
Tareun3406 <Tareun3406@gmail.com>
2022-10-27 21:30:13 +0900

    add admin pages mapping

--- src/main/java/com/recipe/controller/AdminController.java
+++ src/main/java/com/recipe/controller/AdminController.java
@@ -11,4 +11,13 @@ public class AdminController {
     public String adminMain(){
         return "/admin/main";
     }
+
+    @RequestMapping("recipe")
+    public String adminRecipe(){ return "/admin/recipe";}
+
+    @RequestMapping("community")
+    public String adminCommunity(){ return "/admin/community"; }
+
+    @RequestMapping("member")
+    public String adminMember(){ return "/admin/member"; }
 }
---
+++ src/main/webapp/WEB-INF/views/admin/community.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>커뮤니티 관리</title>
+</head>
+<body>
+
+</body>
+</html>

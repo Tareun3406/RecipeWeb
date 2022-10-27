30ffad306a6e77800efc5714190bb14adafa449a
박찬호 <chanho@maegbugpeulo.local>
2022-10-27 20:17:22 +0900

    add authority to admin page

---
+++ src/main/java/com/recipe/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.recipe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/admin")
+@Controller
+public class AdminController {
+
+    @RequestMapping("")
+    public String adminMain(){
+        return "/admin/main";
+    }
+}

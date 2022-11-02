03876f7f9181cb0e32d4e39e7c6010a81a1176ef
박찬호 <chanho@maegbugpeulo.local>
2022-11-02 19:49:48 +0900

    add move url according to permission when logging in add log out and move to the login page when permission is denied

--- src/main/java/com/recipe/security/MemberAccessDeniedHandler.java
+++ src/main/java/com/recipe/security/MemberAccessDeniedHandler.java
@@ -4,8 +4,10 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class MemberAccessDeniedHandler implements AccessDeniedHandler {
@@ -14,6 +16,22 @@ public class MemberAccessDeniedHandler implements AccessDeniedHandler {
     public void handle(HttpServletRequest request, HttpServletResponse response
             , AccessDeniedException accessDeniedException) throws IOException, ServletException {
 
-        response.sendRedirect("/");
+        HttpSession session = request.getSession();
+        session.invalidate();
+
+        deleteRememberMe(request,response);
+
+        response.sendRedirect("/member/login");
+    }
+
+    private void deleteRememberMe(HttpServletRequest request, HttpServletResponse response){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie ck:cookies) {
+            if(ck.getName().equals("remember-me")){
+                ck.setMaxAge(0);
+                response.addCookie(ck);
+                break;
+            }
+        }
     }
 }

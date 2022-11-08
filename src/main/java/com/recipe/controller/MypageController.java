3f4e52b571bf99cf3516af0dc0caed192f274117
gukdongho <109472535+gukdongho@users.noreply.github.com>
2022-11-08 21:38:13 +0900

    last commit

--- src/main/java/com/recipe/controller/MypageController.java
+++ src/main/java/com/recipe/controller/MypageController.java
@@ -29,11 +29,12 @@ public class MypageController {
 
     @RequestMapping("/mypage") //마이페이지 본문
     public String mypage(Model mylistm,Authentication authentication){
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();//시큐리티 인증로그인 할때 저장된 유저이름 가져오기
-        String b=userDetails.getUsername();//가져온 이름 b객체에 저장
-
-        MemberDTO pvo = mypageService.getmylist(b); //저장된 b로 내정보 가져오기
-
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        //시큐리티 인증로그인 할때 저장된 유저이름 가져오기
+        String b=userDetails.getUsername();
+        //가져온 이름 b객체에 저장
+        MemberDTO pvo = mypageService.getmylist(b);
+        //저장된 b로 내정보 가져오기
         mylistm.addAttribute("userlist",pvo);
 
         return "mypage/mypage";
